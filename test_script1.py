191c4e8897904f72392646930addd032f39ff273
dg7541 <danielgodinez@daniels-mbp.lco.gtn>
2017-06-05 11:28:48 -0700

    test script 1

--- test_script1.py
+++ test_script1.py
@@ -20,10 +20,10 @@ class Test(unittest.TestCase):
     def test_compute_statistics(value):
         value.assertEqual( sc.compute_statistics(mjd, mag, magerr), (1.0, 1.0, 1.0, 0.0, 0.0, 0.0, -5.4354180599375495,
                           0.34793189299316019, 1.130441843074534, 31.401821018066236, 0.0097154255645506282, 6798930.9057225743,
-                          1.0119460116981802, 0.0084235779847892222, 1.0, 15.048898825155755), "Statistics array 1 incorrect" )
+                          1.0119460116981802, 0.0084235779847892222, 1.0, 15.048898825155755), "Statistics array incorrect" )
          
     def test_predict_class(value):
-        value.assertEqual( rf.predict_class(mjd, mag, magerr), ['ML'], "Incorrect classification 1")
+        value.assertEqual( rf.predict_class(mjd, mag, magerr), ['ML'], "Incorrect classification")
     
 unittest.main()
 
