6fb803b588f1a0325fdeb2eca1c5436f429cf29f
dg7541 <danielgodinez@Daniels-MacBook-Pro.local>
2017-06-22 16:41:14 -0700

    updated version

--- test/test_script2.py
+++ test/test_script2.py
@@ -4,6 +4,8 @@ Created on Mon Jun  5 11:20:25 2017
 
 @author: danielgodinez
 """
+import sys
+sys.path.append('../')
 import stats_computation as sc
 import random_forest_classifier as rf
 import numpy as np
@@ -11,17 +13,20 @@ import unittest
 
 data = np.loadtxt('TestML_2')
 
-mjd = data[:,0]
 mag = data[:,1]
 magerr = data[:,2]
 
 class Test(unittest.TestCase):
     def test_compute_statistics(value):
-        value.assertEqual( sc.compute_statistics(mjd, mag, magerr), (0.957619477006312, 1.0, 1.0, 0.04238052299368801, 0.0, 0.0, 
-                 -4.116707163123203, 0.54847590773118993, 1.1497516942965478, 23.550972031666717, 0.039397067254302698, 
-                  14788.588739719389, 1.1423033579932351, 0.0062298419394635051, 0.9684400360685302, 18.413412754867913), "Statistics array incorrect" )
+        value.assertEqual( sc.compute_statistics(mag, magerr), (540.89098869488578, 0.87383493175485616, 45.801672292354191, 
+                          -6.4480083062854563, 1.5740162737474053, 517.25538087579378, 0.55438404839089117, 0.9071235347159603, 
+                          0.0053277971807380031, 82, 0.039397067254302698, 0.09804122669179631), "Statistics array incorrect" )
          
     def test_predict_class(value):
-        value.assertEqual( rf.predict_class(mjd, mag, magerr), ['ML'], "Incorrect classification")
+        value.assertEqual( rf.predict_class(mag, magerr)[0], ['ML'], "Incorrect classification")
+        
+    def test_prob_prediction(value):
+        value.assertGreater(rf.predict_class(mag, magerr)[1], 0.5, msg = "Incorrect probability prediction")
     
 unittest.main()
+
