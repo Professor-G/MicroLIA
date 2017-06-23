64c9155942b27f32217cd575e1b8da9782b4a577
Daniel Godines <danielgodinez123@yahoo.com>
2017-06-22 22:49:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="MicrolensingLCOGT",
     version="0.1",
-    description="Microlsening detection algorithm",
+    description="Microlensing detection algorithm",
     author="Daniel Godines Alcantara",
     author_email="dg7541@bard.edu",
     url="https://github.com/dg7541/MicrolensingLCOGT",
