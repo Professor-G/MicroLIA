d36373dd7aa4b6eb3563034c039d0fe429ba8799
dg7541 <danielgodinez@Daniels-MacBook-Pro.local>
2017-06-27 17:20:04 -0700

    updated version

--- test/test_script2.py
+++ test/test_script2.py
@@ -19,8 +19,8 @@ magerr = data[:,2]
 class Test(unittest.TestCase):
     def test_compute_statistics(value):
         value.assertEqual( sc.compute_statistics(mag, magerr), (0.87383493175485616, 45.801672292354191,
-                          -6.4480083062854563, 1.5740162737474053, 517.25538087579378, 0.55438404839089117, 0.9071235347159603, 
-                          0.0053277971807380031, 82, 61, 0.039397067254302698, 0.09804122669179631), "Statistics array incorrect" )
+                          -6.4480083062854563, 1.5740162737474053, 517.25538087579378, 0.55438404839089117, 0.0, 1.0,
+                           0.9071235347159603, 0.027051397655545536, 0.0053277971807380031, 82, 0.09804122669179631), "Statistics array incorrect" )
          
     def test_predict_class(value):
         value.assertEqual( rf.predict_class(mag, magerr)[0], ['ML'], "Incorrect classification")
