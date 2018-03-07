42466d83833e30fa9b9a98ac685537642f4bf34f
dg7541 <danielgodinez@Daniels-MacBook-Pro.local>
2018-03-07 17:52:31 -0500

    real ML event

--- test/test_script2.py
+++ test/test_script2.py
@@ -19,10 +19,10 @@ magerr = data[:,2]
 
 class Test(unittest.TestCase):
     def test_compute_statistics(value):
-        value.assertEqual( sc.compute_statistics(mag, magerr), (10.639980200569692, -1.3951623050335764, -0.88075925533940425, 0.0293252276553836, 4191.4315058255061, 0.58676955737970438, 0.2366412213740458, 0.031868709124813495, 50, 0.26666000000000167, 0.76975206077278036, 1.5553899999999992, 0.059940895316137464), "Statistics array incorrect" )
+        value.assertEqual( sc.compute_statistics(hjd, mag, magerr), (10.639980200569692, -1.3951623050335764, -0.88075925533940425, 0.0293252276553836, 4191.4315058255061, 0.58676955737970438, 0.2366412213740458, 0.031868709124813495, 50, 0.26666000000000167, 0.76975206077278036, 1.5553899999999992, 0.059940895316137464), "Statistics array incorrect" )
          
     def test_predict_class(value):
-        value.assertEqual( rf.predict_class(mag, magerr)[0], ['ML'], "Incorrect classification")
+        value.assertEqual( rf.predict_class(hjd, mag, magerr)[0], ['ML'], "Incorrect classification")
         
 unittest.main()
 
