eea233017d61158cfefa469a0618c88902d256aa
dg7541 <danielgodinez@daniels-mbp.lco.gtn>
2018-08-06 10:43:56 -0700

    fixed typo

--- LIA/test/test_classifier.py
+++ LIA/test/test_classifier.py
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
     Unittest to ensure the classifier is working correctly. 
     """
     def test_predict(value):
-        value.assertEqual( microlensing_classifier.predict(mag,magerr,rf,pca)[0], 'ML', "Classifier failed, predicted class is not corect.")
+        value.assertEqual( microlensing_classifier.predict(mag,magerr,rf,pca)[0], 'ML', "Classifier failed, predicted class is not correct.")
     def test_probability_prediction(value):
         pred = microlensing_classifier.predict(mag,magerr, rf,pca)[1]
         value.assertTrue(pred >= 0.65 and pred <= 0.8, "Classifier failed, probability prediction not within range.")
