d6e290186647ad4d46a300255767ea24799b5b10
dg7541 <danielgodinez@daniels-mbp.lco.gtn>
2018-09-20 16:53:44 -0700

    normalizes flux

--- LIA/lib/extract_features.py
+++ LIA/lib/extract_features.py
@@ -7,6 +7,7 @@
 
 from __future__ import print_function
 from math import log 
+import numpy as np
 from features import *
 
 def extract_all(mag, magerr, convert=True, zp=24):
@@ -44,14 +45,14 @@ def extract_all(mag, magerr, convert=True, zp=24):
     amp = amplitude(flux)
     MedBuffRng = median_buffer_range(flux)
     MedBuffRng2 = median_buffer_range2(flux)
+
     # Normalize by max flux
     norm_flux = flux/np.max(flux)
     norm_fluxerr = flux_err*(norm_flux/flux)
-
+    
     stetJ = stetsonJ(norm_flux,norm_fluxerr)
     stetK = stetsonK(norm_flux,norm_fluxerr)
     stetL = (stetJ*stetK) / 0.798
-    shannon_entr = shannon_entropy(norm_flux,norm_fluxerr)
 
     stats = np.array((above1(norm_flux), above3(norm_flux), above5(norm_flux), abs_energy(norm_flux), abs_sum_changes(norm_flux), amp, 
         auto_corr(norm_flux), below1(norm_flux), below3(norm_flux), below5(norm_flux), c3(norm_flux), check_for_duplicate(norm_flux), check_for_max_duplicate(norm_flux), 
@@ -59,7 +60,7 @@ def extract_all(mag, magerr, convert=True, zp=24):
         count_above(norm_flux), count_below(norm_flux), first_loc_max(norm_flux), first_loc_min(norm_flux), integrate(norm_flux), kurtosis(norm_flux), 
         longest_strike_above(norm_flux), longest_strike_below(norm_flux), mean_abs_change(norm_flux), mean_change(norm_flux), mean_second_derivative(norm_flux), 
         medianAbsDev(norm_flux), MedBuffRng, MedBuffRng2, peak_detection(norm_flux), ratio_recurring_points(norm_flux), 
-        root_mean_squared(norm_flux), sample_entropy(norm_flux), shannon_entr, skewness(norm_flux), np.std(norm_flux), std_over_mean(norm_flux), stetJ, stetK, stetL, 
+        root_mean_squared(norm_flux), sample_entropy(norm_flux), shannon_entropy(norm_flux,norm_fluxerr), skewness(norm_flux), np.std(norm_flux), std_over_mean(norm_flux), stetJ, stetK, stetL, 
         sum_values(norm_flux), time_reversal_asymmetry(norm_flux), vonNeumannRatio(norm_flux)))
     
     stats[np.isinf(stats)] = 0
