ad867b1b5794e066be7383f9709f4b3cb6b70d04
dg7541 <danielgodinez@daniels-mbp.lco.gtn>
2018-09-20 16:52:01 -0700

    now uses 47 pca

--- LIA/models.py
+++ LIA/models.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import decomposition
 
 def create_models(all_feats, pca_feats):
-    """Creates the Random Forest model and PCA transformation used for classificaition.
+    """Creates the Random Forest model and PCA transformation used for classification.
     
     Parameters
     ----------
@@ -26,12 +26,12 @@ def create_models(all_feats, pca_feats):
         PCA transformation.
     """
     coeffs = np.loadtxt(all_feats,usecols=np.arange(2,49))
-    pca = decomposition.PCA(n_components=int(np.ceil(min(coeffs.shape)/2.))+2, whiten=True, svd_solver='auto')
+    pca = decomposition.PCA(n_components=47, whiten=True, svd_solver='auto')
     pca.fit(coeffs)
     #feat_strengths = pca.explained_variance_ratio_
     training_set = np.loadtxt(pca_feats, dtype = str)
-    rf=RandomForestClassifier(n_estimators=1500, max_depth = 16, max_features=5, min_samples_split=16)
-    rf.fit(training_set[:,np.arange(1,27)].astype(float),training_set[:,0])
+    rf=RandomForestClassifier(n_estimators=1600, max_depth = 28, max_features=1, min_samples_leaf = 1, min_samples_split=32)
+    rf.fit(training_set[:,np.arange(1,48)].astype(float),training_set[:,0])
 
     return rf, pca
 
