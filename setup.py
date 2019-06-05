97dd8ee367d302cc073a7037ca2da463c71926bf
dg7541 <danielgodinez@daniels-mbp.lco.gtn>
2019-06-05 13:58:03 -0700

    removed Gatspy dependency

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 3 13:30:11 2018
+
+@author: danielgodinez
+"""
+from setuptools import find_packages, setup
+
+setup(
+	name = "LIA",
+	version = 1.0,
+	description = "Microlensing detection algorithm",
+	author = "Daniel Godinez",
+	author_email = "danielgodinez123@gmail.com",
+	license = 'GPL-3.0',
+	url = "https://github.com/dgodinez77/LIA",
+	install_requires = ['numpy','scikit-learn','astropy','mpmath','scipy','PeakUtils','tsfresh','gatspy','AstroML'],
+	packages = find_packages(),
+	)
