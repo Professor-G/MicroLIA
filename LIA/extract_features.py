c82c7f470344e8113a0c2018925413f72b2956b7
Daniel <daniel@Daniels-MacBook-Pro.local>
2021-09-01 23:26:15 -0700

    derivative space + nan removal

--- LIA/extract_features.py
+++ LIA/extract_features.py
@@ -10,7 +10,7 @@ from math import log
 import numpy as np
 from LIA.features import *
 
-def extract_all(mag, magerr, convert=True, zp=24):
+def extract_all(time, mag, magerr, convert=True, zp=24):
     """This function will compute the statistics used to train the RF.
     Amplitude dependent features are computed first, after which the
     mag/flux is normalized by the maximum value to compute the remanining
@@ -35,6 +35,8 @@ def extract_all(mag, magerr, convert=True, zp=24):
         The 47 features to input into the RF, sorted 
         by alphabetical order. 
     """
+
+
     if convert is True:
         flux = 10**(-(mag-zp)/2.5)
         flux_err = (magerr*flux)/(2.5)*np.log(10)
@@ -62,10 +64,38 @@ def extract_all(mag, magerr, convert=True, zp=24):
         medianAbsDev(norm_flux), MedBuffRng, MedBuffRng2, peak_detection(norm_flux), ratio_recurring_points(norm_flux), 
         root_mean_squared(norm_flux), sample_entropy(norm_flux), shannon_entropy(norm_flux,norm_fluxerr), skewness(norm_flux), np.std(norm_flux), std_over_mean(norm_flux), stetJ, stetK, stetL, 
         sum_values(norm_flux), time_reversal_asymmetry(norm_flux), vonNeumannRatio(norm_flux)))
+
+    flux = np.gradient(flux, time) #derivative
+    flux_err = np.gradient(flux_err, time) #derivative 
+
+    amp = amplitude(flux)
+    MedBuffRng = median_buffer_range(flux)
+    MedBuffRng2 = median_buffer_range2(flux)
+
+    # Normalize by max flux
+    norm_flux = flux/np.max(flux)
+    norm_fluxerr = flux_err*(norm_flux/flux)
+    
+    stetJ = stetsonJ(norm_flux,norm_fluxerr)
+    stetK = stetsonK(norm_flux,norm_fluxerr)
+    stetL = (stetJ*stetK) / 0.798
+
+    stats2 = np.array((above1(norm_flux,norm_fluxerr), above3(norm_flux,norm_fluxerr), above5(norm_flux,norm_fluxerr), abs_energy(norm_flux), abs_sum_changes(norm_flux), amp, 
+        auto_corr(norm_flux), below1(norm_flux,norm_fluxerr), below3(norm_flux,norm_fluxerr), below5(norm_flux,norm_fluxerr), c3(norm_flux), check_for_duplicate(norm_flux), check_for_max_duplicate(norm_flux), 
+        check_for_min_duplicate(norm_flux), check_max_last_loc(norm_flux), check_min_last_loc(norm_flux), complexity(norm_flux), con(norm_flux,norm_fluxerr), con2(norm_flux,norm_fluxerr), 
+        count_above(norm_flux), count_below(norm_flux), first_loc_max(norm_flux), first_loc_min(norm_flux), integrate(norm_flux), kurtosis(norm_flux), 
+        longest_strike_above(norm_flux), longest_strike_below(norm_flux), mean_abs_change(norm_flux), mean_change(norm_flux), mean_second_derivative(norm_flux), 
+        medianAbsDev(norm_flux), MedBuffRng, MedBuffRng2, peak_detection(norm_flux), ratio_recurring_points(norm_flux), 
+        root_mean_squared(norm_flux), sample_entropy(norm_flux), shannon_entropy(norm_flux,norm_fluxerr), skewness(norm_flux), np.std(norm_flux), std_over_mean(norm_flux), stetJ, stetK, stetL, 
+        sum_values(norm_flux), time_reversal_asymmetry(norm_flux), vonNeumannRatio(norm_flux)))
+
+    stats = np.r_[stats, stats2]
     
     stats[np.isinf(stats)] = 0
-    if True in np.isnan(stats):
-        import pdb; pdb.set_trace()
+    stats[np.isnan(stats)] = 0
+
+    #    if True in np.isnan(stats):
+     #   import pdb; pdb.set_trace()
     return stats
 
 
