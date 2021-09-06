119fb6924db296bb971ca319a1f5240853748b4a
Daniel <daniel@Daniels-MacBook-Pro.local>
2021-09-05 18:50:08 -0700

    now using 82 feats

--- LIA/models.py
+++ LIA/models.py
@@ -25,15 +25,15 @@ def create_models(all_feats, pca_feats):
     pca_model : fn
         PCA transformation.
     """
-    coeffs = np.loadtxt(all_feats,usecols=np.arange(2,158))
-    pca = decomposition.PCA(n_components=156, whiten=True, svd_solver='auto')
+    coeffs = np.loadtxt(all_feats,usecols=np.arange(2,84))
+    pca = decomposition.PCA(n_components=82, whiten=True, svd_solver='auto')
     pca.fit(coeffs)
     #feat_strengths = pca.explained_variance_ratio_
     training_set = np.loadtxt(pca_feats, dtype = str)
     training_set = np.loadtxt(all_feats, dtype = str) #testing to see if using only feats works
 
-    rf=RandomForestClassifier(n_estimators=1000)#, max_depth = 4, max_features=2, min_samples_leaf = 4, min_samples_split=2)
-    rf.fit(training_set[:,np.arange(2,158)].astype(float),training_set[:,0])
+    rf=RandomForestClassifier(n_estimators=100)#, max_depth = 4, max_features=2, min_samples_leaf = 4, min_samples_split=2)
+    rf.fit(training_set[:,np.arange(2,84)].astype(float),training_set[:,0])
 
     return rf, pca
 
