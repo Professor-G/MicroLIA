d10cffe2476552bd9c531f9504cc2218cdf40fed
Daniel <daniel@Daniels-MacBook-Pro.local>
2021-09-09 23:31:03 -0700

    NN support

--- LIA/microlensing_classifier.py
+++ LIA/microlensing_classifier.py
@@ -8,7 +8,7 @@ import numpy as np
 from warnings import warn
 from LIA import extract_features
 
-def predict(time, mag, magerr, rf_model, pca_model):
+def predict(time, mag, magerr, model, pca_model):
     """This function uses machine learning to classify any given lightcurve as either
         a cataclysmic variable (CV), a variable source, microlensing, or a constant star 
         displaying no variability.
@@ -21,8 +21,8 @@ def predict(time, mag, magerr, rf_model, pca_model):
         Magnitude array.
     magerr : array
         Corresponing photometric errors.  
-    rf_model: fn
-        The random forest model created using the function in the
+    model: fn
+        The machine learning model created using the function in the
         create_models module.
     pca_model: fn
         The PCA transformation devised using the function in the
@@ -51,8 +51,8 @@ def predict(time, mag, magerr, rf_model, pca_model):
     array.append(extract_features.extract_all(time, mag, magerr, convert=True))
 
     stat_array = pca_model.transform(array)
-    stat_array = array #checking to see if using only stats works
-    pred = rf_model.predict_proba(stat_array)
+    #stat_array = array #checking to see if using only stats works
+    pred = model.predict_proba(stat_array)
     cons_pred, cv_pred, lpv_pred, ml_pred, var_pred = pred[:,0],pred[:,1],pred[:,2],pred[:,3],pred[:,4]
     prediction = classes[np.argmax(pred)]
 
