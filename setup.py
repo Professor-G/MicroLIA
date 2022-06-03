5d9b31793462087d31b6fc5790efec8387c8f0eb
Daniel <daniel@Daniels-MBP-5.MG8702>
2022-06-03 12:00:57 -0600

    version 1.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name="MicroLIA",
-    version="1.61",
+    version="1.70",
     author="Daniel Godines",
     author_email="danielgodinez123@gmail.com",
     description="Machine learning classifier for microlensing",
@@ -19,12 +19,13 @@ setup(
 		'Development Status :: 5 - Production/Stable',
 		'Intended Audience :: Developers',
 		'Topic :: Software Development :: Build Tools',
-                'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Programming Language :: Python :: 3',	   
 ],
     packages=find_packages('.'),
-    install_requires = ['numpy','scikit-learn','astropy','scipy','peakutils', 'progress'],
-    python_requires='>=3.6,<4',
+    install_requires = ['numpy','scikit-learn', 'scikit-optimize', 'astropy','scipy','peakutils',
+        'progress', 'matplotlib', 'missingpy', 'optuna', 'boruta'],
+    python_requires='>=3.7,<4',
     include_package_data=True,
     test_suite="nose.collector",
     package_data={
