f95e9e28e21b0f18525160c1bab7a517164e7ef5
Daniel <daniel@Daniels-MBP-5.MG8702>
2022-07-06 20:28:31 -0600

    new version 1.8.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name="MicroLIA",
-    version="1.8.4",
+    version="1.8.5",
     author="Daniel Godines",
     author_email="danielgodinez123@gmail.com",
     description="Machine learning classifier for microlensing",
