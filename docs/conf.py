caa5824aa90a7e793d4e9bb7cc29d37fbd776138
Daniel <daniel@daniels-mbp-5.mynetworksettings.com>
2023-11-30 00:37:56 -0800

    adding html_css_files

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -96,7 +96,7 @@ pygments_style = None
 html_static_path = ["./source/_static"]
 html_logo = "./source/_static/MicroLIA_logo.png"
 html_theme_options = {"logo_only": True}
-html_css_files = ['theme_overrides.css']
+#html_css_files = ['theme_overrides.css']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
