c446c1fa12b57ea2102708c8337262ff3ded3073
Daniel <daniel@Daniels-MacBook-Pro-10.local>
2025-06-04 00:36:54 -0700

    version 2.7.1 ready, requires python3.9

--- setup.py
+++ setup.py
@@ -4,32 +4,59 @@ Created on Fri Aug 3 13:30:11 2018
 
 @author: danielgodinez
 """
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 setup(
     name="MicroLIA",
-    version="2.7.0",
-    author="Daniel Godines",
+    version="2.7.1",
+    author="Daniel Godinez",
     author_email="danielgodinez123@gmail.com",
-    description="Machine learning classifier for microlensing",
-    license='GPL-3.0',
-    url = "https://github.com/Professor-G/MicroLIA",
+    description="Machine learning classifier for microlensing event detection",
+    long_description="A machine learning pipeline for detecting microlensing events using tree-based models and feature extraction from OGLE data.",
+    license="GPL-3.0",
+    url="https://github.com/Professor-G/MicroLIA",
     classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Programming Language :: Python :: 3',	   
-],
-    packages=find_packages('.'),
-    install_requires = ['numpy','scikit-learn', 'scikit-optimize', 'astropy','scipy','peakutils',
-        'progress', 'matplotlib', 'optuna', 'boruta', 'BorutaShap', 'xgboost', 'scikit-plot',
-        'pandas', 'dill', 'opencv-python', 'gatspy', 'astroML'],
-    python_requires='>=3.7,<4',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scikit-learn==1.1.1",
+        "scikit-optimize==0.9.0",
+        "astropy==5.0.4",
+        "scipy==1.7.3",
+        "peakutils==1.3.3",
+        "progress==1.6",
+        "matplotlib==3.5.1",
+        "optuna==3.1.0",
+        "boruta==0.3",
+        "BorutaShap==1.0.16",
+        "xgboost==1.6.1",
+        "scikit-plot==0.3.7",
+        "opencv-python==4.7.0.68",
+        "pandas==1.4.1",
+        "dill",
+        "gatspy==0.3",
+        "astroML==1.0.2.post1"
+    ],
+    python_requires=">=3.9, <3.10",
     include_package_data=True,
-    test_suite="nose.collector",
     package_data={
-        'MicroLIA': ['data/Miras_vo.xml', 'data/Sesar2010/*', 'test/test_model_xgb/MicroLIA_ensemble_model/*', 'test/test_classifier.py', 
-            'test/test_features.py', 'test/MicroLIA_Training_Set_OGLE_IV.csv', 'test/test_ogle_lc.dat']
+        "MicroLIA": [
+            "data/Miras_vo.xml",
+            "data/Sesar2010/*",
+            "test/test_model_xgb/MicroLIA_ensemble_model/*",
+            "test/test_classifier.py",
+            "test/test_features.py",
+            "test/MicroLIA_Training_Set_OGLE_IV.csv",
+            "test/test_ogle_lc.dat"
+        ]
     },
+    test_suite="nose.collector",
 )
